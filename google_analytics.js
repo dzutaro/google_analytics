132bf176b68ada690f0978447bf8eab85c91ee52
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2016-02-06 23:27:09 -0500

    Add null check.

--- google_analytics.js
+++ google_analytics.js
@@ -4,5 +4,5 @@ dg.modules.google_analytics = new dg.Module();
  * Implements hook_post_process_route_change().
  */
 function google_analytics_post_process_route_change(route, path) {
-  ga('send', 'pageview', '/' + path);
+  if (typeof ga !== 'undefined') { ga('send', 'pageview', '/' + path); }
 }
